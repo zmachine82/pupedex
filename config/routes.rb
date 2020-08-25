cbc6c15d83529d6074ec680e0a282e9a85ce4823
Zack Amis <zachariah.amis@vizientinc.com>
2020-08-24 19:33:12 -0500

    removed routes that were jumping the gun

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "index", to: "puppies#index"
-  get "show", to: "puppies#show"
-  post "create", to: "puppies#create"
-  post "update", to: "puppies#update"
-  post "destroy", to: "puppies#destroy"
+  
   # I hate it ^
 end
